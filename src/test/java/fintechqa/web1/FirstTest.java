c9e4cd8c02c9c04a132d75161c0949fa6a0ed4e7
nox757 <nox757@gmail.com>
2018-11-03 18:40:16 +0300

    Поменяны пути xpath в методе test1 и заменены на css в методе test2
    * использовались ancestor для универсальности, если поменяется
     вложенность тегов на UI (например добавиться один оберточный блок div)
     * css большие и номерами, т.к. нельзя по другому индентифицировать

--- src/test/java/fintechqa/web1/FirstTest.java
+++ src/test/java/fintechqa/web1/FirstTest.java
@@ -11,44 +11,64 @@ public class FirstTest extends BaseRunner {
     @Test
     public void test1() {
         driver.get(baseUrl);
-        driver.findElement(By.name("fio")).click();
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("phone")).click();
-        driver.findElement(By.name("city")).click();
+        driver.findElement(By.xpath("//input[@name='fio']")).click();
+        driver.findElement(By.xpath("//input[@name='email']")).click();
+        driver.findElement(By.xpath("//input[@name='phone']")).click();
+        driver.findElement(By.xpath("//input[@name='city']")).click();
         driver.findElement(By.xpath("//*[@role='listbox' and .//text()='Выберите вакансию']")).click();
-        driver.findElement(By.name("fio")).click();
-        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Фамилия, имя и отчество'])[1]/following::div[3]")).getText());
-        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Электронная почта'])[1]/following::div[3]")).getText());
-        assertEquals("Необходимо указать номер телефона", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Номер телефона'])[1]/following::div[3]")).getText());
-        assertEquals("Поле обязательное", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Город'])[1]/following::div[3]")).getText());
-        assertEquals("Поле обязательное", driver.findElement(By.xpath("//*[@role='listbox' and .//text()='Выберите вакансию']/../following-sibling::*[1]")).getText());
+        driver.findElement(By.xpath("//input[@name='fio']")).click();
+        assertEquals("Поле обязательное",
+                driver.findElement(By.xpath("//*[contains(@class, 'errorMessage') and ./ancestor::*[contains(@class, 'FormField')]//*[@data-qa='fio']]")).getText()
+        );
+        assertEquals("Поле обязательное",
+                driver.findElement(By.xpath("//*[contains(@class, 'errorMessage') and ./ancestor::*[contains(@class, 'FormField')]//*[@data-qa='email']]")).getText()
+        );
+        assertEquals("Необходимо указать номер телефона",
+                driver.findElement(By.xpath("//*[contains(@class, 'errorMessage') and ./ancestor::*[contains(@class, 'FormField')]//input[@name='phone']]")).getText()
+        );
+        assertEquals("Поле обязательное",
+                driver.findElement(By.xpath("//*[contains(@class, 'errorMessage') and ./ancestor::*[contains(@class, 'FormField')]//input[@name='city']]")).getText()
+        );
+        assertEquals("Поле обязательное",
+                driver.findElement(By.xpath("//*[@role='listbox' and .//text()='Выберите вакансию']/ancestor::*[contains(@class, 'FormField')]//*[contains(@class, 'errorMessage')]")).getText()
+        );
     }
 
     @Test
-    public void test2()  {
+    public void test2() {
         driver.get(baseUrl);
-        driver.findElement(By.name("fio")).click();
-        driver.findElement(By.name("fio")).clear();
-        driver.findElement(By.name("fio")).sendKeys("A");
-        driver.findElement(By.name("email")).clear();
-        driver.findElement(By.name("email")).sendKeys("1");
-        driver.findElement(By.name("phone")).click();
-        driver.findElement(By.name("phone")).clear();
-        driver.findElement(By.name("phone")).sendKeys("+7 (2");
-        driver.findElement(By.name("city")).click();
-        driver.findElement(By.name("city")).clear();
-        driver.findElement(By.name("city")).sendKeys("3");
-        driver.findElement(By.name("fio")).click();
-        assertEquals("Недостаточно информации. Введите фамилию, имя и отчество через пробел (Например: Иванов Иван Алексеевич)", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Фамилия, имя и отчество'])[1]/following::div[3]")).getText());
-        assertEquals("Введите корректный адрес эл. почты", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Электронная почта'])[1]/following::div[3]")).getText());
-        assertEquals("Код города/оператора должен начинаться с цифры 3, 4, 5, 6, 8, 9", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Номер телефона'])[1]/following::div[3]")).getText());
-        assertEquals("Допустимо использовать только буквы русского, латинского алфавита и дефис", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Город'])[1]/following::div[3]")).getText());
-        driver.findElement(By.name("phone")).click();
-        driver.findElement(By.name("phone")).clear();
-        driver.findElement(By.name("phone")).sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
-        driver.findElement(By.name("phone")).sendKeys("+7 (989) ");
-        driver.findElement(By.name("fio")).click();
-        assertEquals("Номер телефона должен состоять из 10 цифр, начиная с кода оператора", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Номер телефона'])[1]/following::div[3]")).getText());
+        driver.findElement(By.cssSelector("input[name='fio']")).click();
+        driver.findElement(By.cssSelector("input[name='fio']")).clear();
+        driver.findElement(By.cssSelector("input[name='fio']")).sendKeys("A");
+        driver.findElement(By.cssSelector("input[name='email']")).clear();
+        driver.findElement(By.cssSelector("input[name='email']")).sendKeys("1");
+        driver.findElement(By.cssSelector("input[name='phone']")).click();
+        driver.findElement(By.cssSelector("input[name='phone']")).clear();
+        driver.findElement(By.cssSelector("input[name='phone']")).sendKeys("+7 (2");
+        driver.findElement(By.cssSelector("input[name='city']")).click();
+        driver.findElement(By.cssSelector("input[name='city']")).clear();
+        driver.findElement(By.cssSelector("input[name='city']")).sendKeys("3");
+        driver.findElement(By.cssSelector("input[name='fio']")).click();
+        assertEquals("Недостаточно информации. Введите фамилию, имя и отчество через пробел (Например: Иванов Иван Алексеевич)",
+                driver.findElement(By.cssSelector("[data-qa-file='UIProductBlockForm'] [class*='Row__row']:nth-child(1) [class*='errorMessage']")).getText()
+        );
+        assertEquals("Введите корректный адрес эл. почты",
+                driver.findElement(By.cssSelector("[data-qa-file='UIProductBlockForm'] [class*='Row__row']:nth-child(2) [class*='FormField']:nth-child(1) [class*='errorMessage']")).getText()
+        );
+        assertEquals("Код города/оператора должен начинаться с цифры 3, 4, 5, 6, 8, 9",
+                driver.findElement(By.cssSelector("[data-qa-file='UIProductBlockForm'] [class*='Row__row']:nth-child(2) [class*='FormField']:nth-child(2) [class*='errorMessage']")).getText()
+        );
+        assertEquals("Допустимо использовать только буквы русского, латинского алфавита и дефис",
+                driver.findElement(By.cssSelector("[data-qa-file='UIProductBlockForm'] [class*='Row__row']:nth-child(3) [class*='errorMessage']")).getText()
+        );
+        driver.findElement(By.cssSelector("input[name='phone']")).click();
+        driver.findElement(By.cssSelector("input[name='phone']")).clear();
+        driver.findElement(By.cssSelector("input[name='phone']")).sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        driver.findElement(By.cssSelector("input[name='phone']")).sendKeys("+7 (989) ");
+        driver.findElement(By.cssSelector("input[name='fio']")).click();
+        assertEquals("Номер телефона должен состоять из 10 цифр, начиная с кода оператора",
+                driver.findElement(By.cssSelector("[data-qa-file='UIProductBlockForm'] [class*='Row__row']:nth-child(2) [class*='FormField']:nth-child(2) [class*='errorMessage']")).getText()
+        );
     }
 
 }
